f487bf520d7039e160f5fb8d7275cab36ab304ac
mkhoyanarmen <93216230+mkhoyanarmen@users.noreply.github.com>
2023-06-07 20:17:47 +0400

    Add project files.

---
+++ Xndirner_281-290/Program.cs
@@ -0,0 +1,410 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xndirner_281_290
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //281
+            //Random rnd = new Random();
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] x = new int[n];
+            //Console.WriteLine("---------X---------");
+            //int count = 0;
+            //for (int i = 0; i < x.Length; i++)
+            //{
+            //    x[i] = rnd.Next(-10, 20);
+            //    if (x[i] > 0)
+            //    {
+            //        count++;
+            //    }
+            //    Console.Write(x[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int[] y = new int[count];
+
+            //Console.WriteLine("---------Y---------");
+            //int j = 0;
+            //for (int i = 0; i < x.Length; i++)
+            //{
+            //    if (x[i] > 0)
+            //    {
+            //        y[j] = x[i];
+            //        Console.Write(y[j] + " ");
+            //        j++;
+            //    }
+            //}
+            //Console.WriteLine();
+
+            //282
+            //Random rnd = new Random();
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] x = new int[n];
+            //Console.WriteLine("---------X---------");
+            //int count = 0;
+            //for (int i = 0; i < x.Length; i++)
+            //{
+            //    x[i] = rnd.Next(-10, 20);
+            //    if (x[i] > 0 || x[i] < 0)
+            //    {
+            //        count++;
+            //    }
+            //    Console.Write(x[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int[] y = new int[count];
+
+            //Console.WriteLine("---------Y---------");
+            //int j = 0;
+            //for (int i = 0; i < x.Length; i++)
+            //{
+            //    if (x[i] > 0 || x[i] < 0)
+            //    {
+            //        y[j] = x[i];
+            //        Console.Write(y[j] + " ");
+            //        j++;
+            //    }
+            //}
+            //Console.WriteLine();
+
+            //283
+            //Random rnd = new Random();
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] x = new int[n];
+            //Console.WriteLine("---------X---------");
+            //int count = 0;
+            //for (int i = 0; i < x.Length; i++)
+            //{
+            //    x[i] = rnd.Next(-10, 20);
+            //    if (x[i] % 2 != 0)
+            //    {
+            //        count++;
+            //    }
+            //    Console.Write(x[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int[] y = new int[count];
+
+            //Console.WriteLine("---------Y---------");
+            //int j = 0;
+            //for (int i = 0; i < x.Length; i++)
+            //{
+            //    if (x[i] % 2 != 0)
+            //    {
+            //        y[j] = x[i];
+            //        Console.Write(y[j] + " ");
+            //        j++;
+            //    }
+            //}
+            //Console.WriteLine();
+
+            //284
+            //Random rnd = new Random();
+            //int n, a, b;
+            //do
+            //{
+            //    Console.Write("mutqagrel a: ");
+            //    a = int.Parse(Console.ReadLine());
+            //    Console.Write("mutqagrel b: ");
+            //    b = int.Parse(Console.ReadLine());
+            //} while (!(a < b));
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] x = new int[n];
+            //Console.WriteLine("---------X---------");
+            //int count = 0;
+            //for (int i = 0; i < x.Length; i++)
+            //{
+            //    x[i] = rnd.Next(-10, 20);
+            //    if (x[i] > a && x[i] < b)
+            //    {
+            //        count++;
+            //    }
+            //    Console.Write(x[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int[] y = new int[count];
+
+            //Console.WriteLine("---------Y---------");
+            //int j = 0;
+            //for (int i = 0; i < x.Length; i++)
+            //{
+            //    if (x[i] > a && x[i] < b)
+            //    {
+            //        y[j] = x[i];
+            //        Console.Write(y[j] + " ");
+            //        j++;
+            //    }
+            //}
+            //Console.WriteLine();
+
+            //285
+            //Random rnd = new Random();
+            //Console.WriteLine("Mutqagrel p tivy");
+            //int p = int.Parse(Console.ReadLine());
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] x = new int[n];
+            //Console.WriteLine("---------X---------");
+            //int count = 0;
+            //for (int i = 0; i < x.Length; i++)
+            //{
+            //    x[i] = rnd.Next(-10, 20);
+            //    if (x[i] % p == 0)
+            //    {
+            //        count++;
+            //    }
+            //    Console.Write(x[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int[] y = new int[count];
+
+            //Console.WriteLine("---------Y---------");
+            //int j = 0;
+            //for (int i = 0; i < x.Length; i++)
+            //{
+            //    if (x[i] % p == 0)
+            //    {
+            //        y[j] = x[i];
+            //        Console.Write(y[j] + " ");
+            //        j++;
+            //    }
+            //}
+            //Console.WriteLine();
+
+            //286
+            //Random rnd = new Random();
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] x = new int[n];
+            //Console.WriteLine("---------X---------");
+            //int count = 0;
+            //for (int i = 0; i < x.Length; i++)
+            //{
+            //    x[i] = rnd.Next(-10, 20);
+            //    if (x[i] % 2 == 0)
+            //    {
+            //        count++;
+            //    }
+            //    Console.Write(x[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int[] y = new int[count];
+
+            //Console.WriteLine("---------Y---------");
+            //int j = 0;
+            //for (int i = 0; i < x.Length; i++)
+            //{
+            //    if (x[i] % 2 == 0)
+            //    {
+            //        y[j] = x[i];
+            //        Console.Write(y[j] + " ");
+            //        j++;
+            //    }
+            //}
+            //Console.WriteLine();
+
+            //287
+            //Random rnd = new Random();
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] x = new int[n];
+            //Console.WriteLine("---------X---------");
+            //int count = 0;
+            //for (int i = 0; i < x.Length; i++)
+            //{
+            //    x[i] = rnd.Next(-10, 20);
+            //    if (x[i] != i)
+            //    {
+            //        count++;
+            //    }
+            //    Console.Write(x[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int[] y = new int[count];
+
+            //Console.WriteLine("---------Y---------");
+            //int j = 0;
+            //for (int i = 0; i < x.Length; i++)
+            //{
+            //    if (x[i] != i)
+            //    {
+            //        y[j] = x[i];
+            //        Console.Write(y[j] + " ");
+            //        j++;
+            //    }
+            //}
+            //Console.WriteLine();
+
+            //288
+            //Random rnd = new Random();
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] x = new int[n];
+            //Console.WriteLine("---------X---------");
+            //int count = 0;
+            //for (int i = 0; i < x.Length; i++)
+            //{
+            //    x[i] = rnd.Next(-10, 20);
+            //    if (i % 2 != 0)
+            //    {
+            //        count++;
+            //    }
+            //    Console.Write(x[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int[] y = new int[count];
+
+            //Console.WriteLine("---------Y---------");
+            //int j = 0;
+            //for (int i = 0; i < x.Length; i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        y[j] = x[i];
+            //        Console.Write(y[j] + " ");
+            //        j++;
+            //    }
+            //}
+            //Console.WriteLine();
+
+            //289
+            //Random rnd = new Random();
+            //int n, c, d;
+            //do
+            //{
+            //    Console.WriteLine("mutqagrel c: ");
+            //    c = int.Parse(Console.ReadLine());
+            //    Console.WriteLine("mutqagrel d: ");
+            //    d = int.Parse(Console.ReadLine());
+            //} while (!(c < d));
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] x = new int[n];
+            //Console.WriteLine("---------X---------");
+            //int count = 0;
+            //for (int i = 0; i < x.Length; i++)
+            //{
+            //    x[i] = rnd.Next(-10, 20);
+            //    if (x[i] * x[i] < d && x[i] * x[i] > c)
+            //    {
+            //        count++;
+            //    }
+            //    Console.Write(x[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int[] y = new int[count];
+
+            //Console.WriteLine("---------Y---------");
+            //int j = 0;
+            //for (int i = 0; i < x.Length; i++)
+            //{
+            //    if (x[i] * x[i] < d && x[i] * x[i] > c)
+            //    {
+            //        y[j] = x[i];
+            //        Console.Write(y[j] + " ");
+            //        j++;
+            //    }
+            //}
+            //Console.WriteLine();
+
+            //290
+            Random rnd = new Random();
+            int n;
+            do
+            {
+                Console.WriteLine("Mutqagrel zangvaci chapy");
+                n = int.Parse(Console.ReadLine());
+            } while (!(n > 0));
+
+            int[] x = new int[n];
+            Console.WriteLine("---------X---------");
+            int count = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                x[i] = rnd.Next(-10, 20);
+                if (x[i] % 6 == 1)
+                {
+                    count++;
+                }
+                Console.Write(x[i] + " ");
+            }
+            Console.WriteLine();
+
+            int[] y = new int[count];
+
+            Console.WriteLine("---------Y---------");
+            int j = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (x[i] % 6 == 1)
+                {
+                    y[j] = x[i];
+                    Console.Write(y[j] + " ");
+                    j++;
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
